249cd79416569dd54ec7298104ea7095b538b555
Кирилл Романенко <romanenko.kirill1995@gmail.com>
2020-04-24 15:23:31 +0300

    small fix

--- renderer/addSizeWord.js
+++ renderer/addSizeWord.js
@@ -11,28 +11,3 @@ document.getElementById('addSizeForm').addEventListener('submit', function (e) {
     ipcRenderer.send('add-sizeWord-form', data);
     $('#addSizeForm')[0].reset();
 })
-
-
-
-// $('#buttonSend').on('click', (e) => {
-//     e.preventDefault()
-//     let size;
-//     size = Number($('#inputSize').val());
-//     $('#inputSize').val('');
-//     if (size > 0) {
-//         console.log(size);
-//         ipcRenderer.send('add-sizeWord-form', size);
-//         // let countStrings = functions.getRandom(100, 500);
-//         // let text = [];
-//         // text = functions.generateText(text, countStrings, size);
-//         // let polindromArray = [];
-//         // polindromArray = functions.polindromText(text, polindromArray);
-//         // const textRedis = JSON.stringify(text);
-//         // const polindromRedis = JSON.stringify(polindromArray);
-//         // request.setCountWords(countStrings);
-//         // request.setSourceText(textRedis);
-//         // request.setPolindrom(polindromRedis);
-//     }
-
-
-// })
